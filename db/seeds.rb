4f3171a5e0d5cd8730440d2e402a05039f4779e0
Karl Heitmann <khf1988@gmail.com>
2021-05-01 23:06:46 -0400

    creado el scaffold para posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |i|
+    Post.create(title: "Post #{i+1}", body: Faker::Lorem.paragraph(sentence_count: 500))
+end
